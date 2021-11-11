10c02805577f59059bde1d8cac593dd90f1838ea
palagashviliniika <n_palagashvili@cu.edu.ge>
2021-11-11 19:10:43 +0400

    Finished form validation and used RegX

--- add.php
+++ add.php
@@ -15,21 +15,30 @@
         if (empty($_POST['email'])){
             echo "Email is required <br/>";
         }else{
-            echo htmlspecialchars($_POST['email']);
+            $email = $_POST['email'];
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)){  //it validates email!!
+                echo "Email must be a valid email address <br/>";
+            }
         }
 
         //        check title
         if (empty($_POST['title'])){
             echo "Title is required <br/>";
         }else{
-            echo htmlspecialchars($_POST['title']);
+            $title = $_POST['title'];
+            if (!preg_match('/^[a-zA-Z\s\']+$/', $title)){  //it checks if the title has a-z, A-Z, spaces, ' - s
+                echo "Title must be letters and spaces only <br/>";
+            }
         }
 
         //        check ingredients
         if (empty($_POST['ingredients'])){
             echo "At least one ingredient is required <br/>";
         }else{
-            echo htmlspecialchars($_POST['ingredients']);
+            $ingredients = $_POST['ingredients'];
+            if (!preg_match('/^([a-zA-Z ]+)(,\s*[a-zA-Z]*)*$/', $ingredients)){
+                echo "Ingredients must be a comma separated list <br/>";
+            }
         }
     } // end of the basic POST check. If the want to check whether html form inputs are empty, we can use "required" attribute in them
 
@@ -44,7 +53,7 @@
     <h4 class="center">Add a Pizza</h4>
     <form action="add.php" method="POST" class="white">
         <label for="">Your Email</label>               <!--  we must put id in for-->
-        <input type="email" name="email" required>
+        <input type="text" name="email" required>
         <label for="">Pizza Title</label>
         <input type="text" name="title">
         <label for="">Ingredients (comma separated):</label>
