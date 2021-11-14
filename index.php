2c8278a5e0ec2ab1b93a4c3e6cce3c6cb7ae9513
palagashviliniika <n_palagashvili@cu.edu.ge>
2021-11-14 07:06:32 -0800

    changed the loops and ifs syntax

--- index.php
+++ index.php
@@ -42,16 +42,16 @@ mysqli_close($conn);
 <div class="container">
     <div class="row">
 
-        <?php foreach ($pizzas as $pizza){ ?>
+        <?php foreach ($pizzas as $pizza): ?>
 
             <div class="col s6 md3">
                 <div class="card z-depth-0">
                     <div class="card-content center">
                         <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
                         <ul>
-                            <?php foreach (explode(',', $pizza['ingredients']) as $ing) {?>
+                            <?php foreach (explode(',', $pizza['ingredients']) as $ing) :?>
                                 <li><?php echo htmlspecialchars($ing); ?></li>
-                            <?php } ?>
+                            <?php endforeach; ?>
                         </ul>
                     </div>
                     <div class="card-action right-align">
@@ -60,7 +60,13 @@ mysqli_close($conn);
                 </div>
             </div>
 
-        <?php } ?>
+        <?php endforeach; ?>
+
+        <?php if (count($pizzas) >= 2) : ?>
+            <p>There are 2 or more pizzas</p>
+        <?php  else : ?>
+            <p>There are less than 2 pizzas</p>
+        <?php endif; ?>
 
     </div>
 </div>
