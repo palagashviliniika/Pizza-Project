b7003a1ccbaccebd85ec0f8db202e97a060bb5fd
palagashviliniika <n_palagashvili@cu.edu.ge>
2021-11-15 06:15:53 -0800

    Added Delete function

--- details.php
+++ details.php
@@ -2,6 +2,20 @@
 
 include "config/db_connect.php";
 
+if (isset($_POST['delete'])){
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+    $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+    if(mysqli_query($conn, $sql)){
+        //success
+        header('Location: index.php');
+    } else {
+        //failure
+        echo "Query error: ". mysqli_error($conn);
+    }
+}
+
 // check GET request id parameter
 
 if (isset($_GET['id'])){
@@ -41,6 +55,11 @@ if (isset($_GET['id'])){
             <h5> Ingredients: </h5>
             <p> <?php echo htmlspecialchars($pizza['ingredients']); ?> </p>
 
+            <!-- Delete Form           -->
+            <form action="details.php" method="POST">
+                <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
+                <input type="submit" name="delete" value="Delete" class="btn brand  z-depth-0">
+            </form>
 
         <?php else: ?>
 
