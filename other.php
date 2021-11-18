de93576de8bcea10c3a30ca74894fdb4b0d6d292
palagashviliniika <n_palagashvili@cu.edu.ge>
2021-11-18 09:01:06 -0800

    Added OOP basics and finished the PHP toturial

--- other.php
+++ other.php
@@ -33,10 +33,10 @@
 
 //file system - part 2
 
-$file = 'quotes.txt';
+//$file = 'quotes.txt';
 
 //opening a file for reading
-$handle = fopen($file,'a+');
+//$handle = fopen($file,'a+');
 
 //read the file
 //echo fread($handle, filesize($file));
@@ -54,10 +54,64 @@ $handle = fopen($file,'a+');
 //fwrite($handle, "\nEverything popular is wrong");
 
 //closing a file
-fclose($handle);
+//fclose($handle);
 
 //delete the file
-unlink('quotes.txt');
+//unlink('quotes.txt');
 
+
+
+// CLASSES!!!
+
+//First we create a class and write its properties and functions
+class User {
+
+    private $email;
+    private $name;
+
+    //we give initial values for the new objects of this class
+    public function __construct($name, $email)
+    {
+//        $this->email = 'mario@thenetninja.co.uk';
+//        $this->name = 'Mario';
+        $this->email = $email;
+        $this->name = $name;
+    }
+
+    public function login(){
+        echo $this->name . " logged in";
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function setName($name){
+        if(is_string($name) && strlen($name) > 1){
+            $this->name = $name;
+            return "Name has been updated to $name";
+        } else {
+            return 'Not a valid name';
+        }
+    }
+
+}
+
+//Now we create an object
+//$userOne = new User(); // its called class instantiation
+//
+//$userOne->login();
+//echo $userOne->name;
+
+$userTwo = new User('Nika','palagashvilinika@gmail.com');
+//echo $userTwo->name . '</br>';
+//$userTwo->name = 'Mario';
+//echo $userTwo->email . '</br>';
+//echo $userTwo->login();
+
+echo $userTwo->getName() . '</br>';
+//echo $userTwo->setName(50) . '</br>';
+echo $userTwo->setName('Mario') . '</br>';
+echo $userTwo->getName();
 
 ?>
